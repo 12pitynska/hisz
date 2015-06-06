dc844321996a32d4ec5a7ee87c65e81e4e6a205c
Aneta Pityńska <aneta.pitynska@no-restrictions.com>
2015-06-06 14:21:40 +0200

    links styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
+
+
+
+$(document).ready(function(){$('#sidebar').affix({
+      offset: {
+       top: 100
+      }
+});	
+})
