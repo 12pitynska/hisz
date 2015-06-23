c8ef08ee5bfba54fe4cbec111bcb81defbeba14b
Aneta Pityńska <aneta.pitynska@no-restrictions.com>
2015-06-23 20:17:36 +0200

    s

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -14,7 +14,7 @@ class NewsController < ApplicationController
   def index
     @news = News.all.order('created_at DESC').page(params[:page]).per(5)
     @links = Link.all.count
-        @archnews = News.all.order('created_at ASC').last(4).reverse
+    @archnews = News.all.order('created_at ASC').last(4).reverse
 
   end
 
