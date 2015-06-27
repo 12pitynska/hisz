dd5e284ad9175cb10e5c7312484247d31596540e
Aneta Pityńska <aneta.pitynska@no-restrictions.com>
2015-06-27 14:39:43 +0200

    redirect to current after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,35 +3,12 @@ class ApplicationController < ActionController::Base
 	helper :all
 	 # Prevent CSRF attacks by raising an exception.
      # For APIs, you may want to use :null_session instead.
-
-
-before_filter :store_location
-
 before_action :configure_permitted_parameters, if: :devise_controller?
 protect_from_forgery with: :exception
 rescue_from ActionController::RoutingError, :with => :record_not_found
 rescue_from ActiveRecord::RecordNotFound, :with => :not_found
 def routing_error
     raise ActionController::RoutingError.new(params[:path])
-end
-
-
-def store_location
-  # store last url - this is needed for post-login redirect to whatever the user last visited.
-  return unless request.get? 
-  if (request.path != "/users/sign_in" &&
-      request.path != "/users/sign_up" &&
-      request.path != "/users/password/new" &&
-      request.path != "/users/password/edit" &&
-      request.path != "/users/confirmation" &&
-      request.path != "/users/sign_out" &&
-      !request.xhr?) # don't store ajax calls
-    session[:previous_url] = request.fullpath 
-  end
-end
-
-def after_sign_in_path_for(resource)
-  session[:previous_url] || root_path
 end
 
 
