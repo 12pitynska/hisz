b8cb02f728ef2b7c3fa02adc0827aafe6aa6ebd6
Aneta Pityńska <aneta.pitynska@no-restrictions.com>
2015-06-27 14:33:20 +0200

    redirect to current after login

--- app/controllers/my_devise/registrations_controller.rb
+++ app/controllers/my_devise/registrations_controller.rb
@@ -6,5 +6,13 @@ class MyDevise::RegistrationsController < Devise::RegistrationsController
 		def commitee_params
 			     params.require(:user).permit(:email, :password, :password_confirmation, :login, :role_id)
 		end
+end
+ protected
+    def after_sign_up_path_for(resource)
+      signed_in_root_path(resource)
+    end
 
+    def after_update_path_for(resource)
+      signed_in_root_path(resource)
+    end
 end 
